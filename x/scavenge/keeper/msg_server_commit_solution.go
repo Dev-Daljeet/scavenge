e96eaea1de8a52cc9a77844c486ab3beefa23bd4
Dev-Daljeet (Daljeet Singh) <daljeet@devdaljeet.com>
2022-03-23 13:17:23 -0400

    Implement the keepers

--- x/scavenge/keeper/msg_server_commit_solution.go
+++ x/scavenge/keeper/msg_server_commit_solution.go
@@ -3,15 +3,26 @@ package keeper
 import (
 	"context"
 
+	"github.com/cosmonaut/scavenge/x/scavenge/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/devdaljeet/scavenge/x/scavenge/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 func (k msgServer) CommitSolution(goCtx context.Context, msg *types.MsgCommitSolution) (*types.MsgCommitSolutionResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
-
-	// TODO: Handling the message
-	_ = ctx
-
+  // create a new commit from the information in the MsgCommitSolution message
+	var commit = types.Commit{
+		Index:                 msg.SolutionScavengerHash,
+		SolutionHash:          msg.SolutionHash,
+		SolutionScavengerHash: msg.SolutionScavengerHash,
+	}
+  // try getting a commit from the store using the solution+scavenger hash as the key
+	_, isFound := k.GetCommit(ctx, commit.SolutionScavengerHash)
+	// return an error if a commit already exists in the store
+	if isFound {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "Commit with that hash already exists")
+	}
+  // write commit to the store
+	k.SetCommit(ctx, commit)
 	return &types.MsgCommitSolutionResponse{}, nil
 }
--- x/scavenge/keeper/msg_server_submit_scavenge.go
+++ x/scavenge/keeper/msg_server_submit_scavenge.go
@@ -1,17 +1,48 @@
 package keeper
 
 import (
-	"context"
+  "context"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/devdaljeet/scavenge/x/scavenge/types"
+  "github.com/cosmonaut/scavenge/x/scavenge/types"
+  sdk "github.com/cosmos/cosmos-sdk/types"
+  sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+  "github.com/tendermint/tendermint/crypto"
 )
 
 func (k msgServer) SubmitScavenge(goCtx context.Context, msg *types.MsgSubmitScavenge) (*types.MsgSubmitScavengeResponse, error) {
+  // get context that contains information about the environment, such as block height
 	ctx := sdk.UnwrapSDKContext(goCtx)
-
-	// TODO: Handling the message
-	_ = ctx
-
+  // create a new scavenge from the data in the MsgSubmitScavenge message
+	var scavenge = types.Scavenge{
+		Index:        msg.SolutionHash,
+		Description:  msg.Description,
+		SolutionHash: msg.SolutionHash,
+		Reward:       msg.Reward,
+	}
+  // try getting a scavenge from the store using the solution hash as the key
+	_, isFound := k.GetScavenge(ctx, scavenge.SolutionHash)
+  // return an error if a scavenge already exists in the store
+	if isFound {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "Scavenge with that solution hash already exists")
+	}
+  // get address of the Scavenge module account
+	moduleAcct := sdk.AccAddress(crypto.AddressHash([]byte(types.ModuleName)))
+  // convert the message creator address from a string into sdk.AccAddress
+	scavenger, err := sdk.AccAddressFromBech32(scavenge.Scavenger)
+	if err != nil {
+		panic(err)
+	}
+  // convert tokens from string into sdk.Coins
+	reward, err := sdk.ParseCoinsNormalized(scavenge.Reward)
+	if err != nil {
+		panic(err)
+	}
+  // send tokens from the scavenge creator to the module account
+	sdkError := k.bankKeeper.SendCoins(ctx, scavenger, moduleAcct, reward)
+	if sdkError != nil {
+		return nil, sdkError
+	}
+  // write the scavenge to the store
+	k.SetScavenge(ctx, scavenge)
 	return &types.MsgSubmitScavengeResponse{}, nil
 }
